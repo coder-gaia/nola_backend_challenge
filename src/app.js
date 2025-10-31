6aaeaf58353733ed5fedb459b4cae2bc01dafe2b
coder-gaia <alexandregaia911@gmail.com>
2025-10-30 21:57:51 -0300

    agora vai

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes/index.js";
@@ -7,25 +6,29 @@ import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(express.json());
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://nola-frontend-r93whj8fo-codergaias-projects.vercel.app",
+  "https://nola-frontend-9y7bxfqtg-codergaias-projects.vercel.app",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 
 app.use(helmet());
 app.use(morgan("dev"));
