4337cb59daabd5d38edc595909e7aae5ad9b2610
coder-gaia <alexandregaia911@gmail.com>
2025-10-31 12:05:38 -0300

    added local variable

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,17 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
+const connectionString =
+  process.env.NODE_ENV === "development"
+    ? process.env.DEV_DATABASE
+    : process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === "development"
+      ? false
+      : { rejectUnauthorized: false },
 });
 
 export const query = (text, params) => pool.query(text, params);
